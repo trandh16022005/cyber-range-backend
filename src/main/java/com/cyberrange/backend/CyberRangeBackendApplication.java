84c5b0a43289fdcbaf801cd2eeeab9d923575dc9
trandh16022005 <trandh16022005@gmail.com>
2026-08-28 22:35:37 +0700

    Initial Spring Boot project

---
+++ src/main/java/com/cyberrange/backend/CyberRangeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cyberrange.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyberRangeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CyberRangeBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cyberrange/backend/CyberRangeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cyberrange.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CyberRangeBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
